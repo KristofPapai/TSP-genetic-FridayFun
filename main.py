6d003c861aa6d42d848e4771f54be44a5449f326
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2021-11-19 12:02:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ cities_cordinates = [[4,3],[2,4],[9,3],[7,2],[1,6],[3,11]]
 
 
 def generateGnomes(genNumber):
-    #distanceMatrix(randomGnom(len(cities_cordinates)))
+    tempTable = []
     gnomTable = []
     for i in range(0,genNumber):
         tempGnom = randomGnom(len(cities_cordinates))
         tempGnom.append(distanceMatrix(tempGnom))
         gnomTable.append(tempGnom)
+    #print(*tempTable, sep='\n')
     return gnomTable
 
 def randomGnom(arrLength):
@@ -30,4 +31,27 @@ def distanceMatrix(gnomArr):
         calcLength += matrix[i][gnomArr[i]]
     return calcLength
 
+
+def gnomeSelection(gnomeTable):
+    newGnomeTable = []
+    tempData1Table = []
+    tempData2Table = []
+    tempIndex = []
+
+    for i in range(0,len(gnomeTable)):
+        tempData1Table.append(gnomeTable[i][6])
+    print(tempData1Table)
+    tempData2Table = sorted(tempData1Table)[:5]
+    print(tempData2Table)
+    #ahol a tempdata 1 értéke egyenlőa tempdata2-vel
+    for i in range(0,len(tempData2Table)):
+        for x in range(0,len(tempData1Table)):
+            if tempData2Table[i] == tempData1Table[x]:
+                tempIndex.append(x)
+    print(tempIndex)
+    
+    return newGnomeTable
+
 print(*generateGnomes(10), sep='\n')
+print("-----------------")
+print(*gnomeSelection(generateGnomes(10)),sep='\n')
